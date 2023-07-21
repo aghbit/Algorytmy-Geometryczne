acf31b8d4d4a45ff3516b771400f4b7102791d20
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-07-21 22:41:47 +0200

    test core update

--- bitalg/tests/test_core.py
+++ bitalg/tests/test_core.py
@@ -8,14 +8,34 @@ class TestCore:
     # się dogadać jak ma wyglądać budowa drzewo folderów.
 
     def test(self, lab_no, task_no, func):
-        print("Lab " + str(lab_no) + str(", task ") + str(task_no) + ":")
+        print("Lab {}, task {}:".format(lab_no, task_no))
 
+        counter = 0
         for test_no in range(1, self.tests_in[lab_no - 1][task_no - 1] + 1):
-            print("\tTest: " + str(test_no), end=": ")
+            print("\tTest {}:".format(test_no), end=" ")
 
             result = func(lab_no, task_no, test_no)
 
-            if result:
+            if result == 1:
                 print("Passed")
+                counter += 1
+            #elif result == 0:
+            #    print("TIMEOUT")
             else:
                 print("WRONG ANSWER")
+
+        print("Result {}/{}".format(counter, self.tests_in[lab_no - 1][task_no - 1]))
+
+def m(lab, task, test):
+    res = 1
+    list = open("test{}_tests\\task{}\\test_{}_{}_{}.in".format(lab, task, lab, task, test), "r").read().split(" ")
+    for i in list:
+        res *= int(i)
+    if str(res) == open("test{}_tests\\task{}\\test_{}_{}_{}.out".format(lab, task, lab, task, test), "r").read():
+        return True
+    else:
+        return False
+
+x = TestCore()
+x.test(3,1,m)
+
