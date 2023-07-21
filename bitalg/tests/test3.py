5efd2002b92c7a5552ca50c3ecc48d8074ea1b48
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-07-21 22:42:15 +0200

    test3 build

--- bitalg/tests/test3.py
+++ bitalg/tests/test3.py
@@ -1,8 +1,17 @@
 from test_core import TestCore
 
-class Test(TestCore):git
+class Test(TestCore):
     def runtest(self, lab_no, task_no):
+        pass
+
+    #is y-monotone
+    def task2_func(self, test_no):
         ...
 
-    def zad1_func(self, lab_no, task_no, test_no):
+    # vertex classification (początkowe, końcowe, łączące, dzielące i prawidłowe)
+    def task3_func(self, test_no):
+        ...
+
+    # triangulation with steps
+    def task4_func(self, test_no):
         ...
