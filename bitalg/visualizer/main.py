a44011b1f0e24cfe1b225ec508ca551ebb09e180
c-i-a-s-t-e-k <jezowdominik@gmail.com>
2023-07-25 15:20:01 +0200

    save_plot almost done open_plot started

--- bitalg/visualizer/main.py
+++ bitalg/visualizer/main.py
@@ -32,3 +32,26 @@ class Visualizer():
         ax.autoscale()
 
         fig.show(warn=False)
+
+    # save informations about points and line segments of current Visualizer in file of given name $file_name
+    def save_plot(self, file_name):
+        with open(file_name, "w") as file:
+            file.write("points\n")
+            for points, color in self.points_array:
+                file.write(color)
+                file.write(points)
+            file.write("\nline_segments\n")
+            for line_segments, color in self.line_segments_array:
+                file.write(color)
+                file.write(line_segments)
+        
+    
+    def _clear(self):
+        self.points_array.clear()
+        self.line_segments_array.clear()
+    
+    # clear current visualizer and fill it with elements from file of given name
+    def open_plot(self, file_name):
+        self._clear()
+        with open(file_name, "r") as file:
+            raise RuntimeError("not implement yet")
