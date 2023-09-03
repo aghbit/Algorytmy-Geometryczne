72af47f1b20bd8333103714df7d3e0e554a91a92
jakmro <kubamroz124@gmail.com>
2023-09-03 20:22:24 +0200

    Adding add_title and add_grid functionality

--- bitalg/visualizer/plot/plot.py
+++ bitalg/visualizer/plot/plot.py
@@ -6,20 +6,32 @@ import os
 
 class Plot:
     @staticmethod
-    def __build_plot(data):
+    def __build_plot(plot_data, data):
         fig, ax = plt.subplots()
         ax.set_xlabel('x')
         ax.set_ylabel('y')
+
+        if 'title' in plot_data:
+            ax.set_title(plot_data['title'])
+        if 'grid' in plot_data:
+            ax.grid()
+
         for figure in data:
             figure.draw(ax)
         ax.autoscale()
         return fig, ax
 
     @staticmethod
-    def __build_gif(data, interval):
+    def __build_gif(plot_data, data, interval):
         fig, ax = plt.subplots()
         artists = []
         frames = [ax.set_xlabel('x'), ax.set_ylabel('y')]
+
+        if 'title' in plot_data:
+            ax.set_title(plot_data['title'])
+        if 'grid' in plot_data:
+            ax.grid()
+
         for figure in data:
             artist = figure.draw(ax)
             frames.extend(artist)
@@ -28,26 +40,27 @@ class Plot:
         return animation.ArtistAnimation(fig=fig, artists=artists, interval=interval, blit=True)
 
     @staticmethod
-    def show(data):
-        fig, _ = Plot.__build_plot(data)
+    def show(plot_data, data):
+        fig, _ = Plot.__build_plot(plot_data, data)
         fig.show(warn=False)
 
     @staticmethod
-    def save(data, filename):
-        fig, _ = Plot.__build_plot(data)
+    def save(plot_data, data, filename):
+        fig, _ = Plot.__build_plot(plot_data, data)
         fig.savefig(filename)
         plt.close()
 
     @staticmethod
-    def show_gif(data, interval):
-        Plot.save_gif(data, interval, f'{__file__}.__tmp_animation_holder__')
+    def show_gif(plot_data, data, interval):
+        Plot.save_gif(plot_data, data, interval,
+                      f'{__file__}.__tmp_animation_holder__')
         plt.close()
         gif = Image(f'{__file__}.__tmp_animation_holder__.gif')
         os.remove(f'{__file__}.__tmp_animation_holder__.gif')
         return gif
 
     @staticmethod
-    def save_gif(data, interval, filename):
-        anim = Plot.__build_gif(data, interval)
+    def save_gif(plot_data, data, interval, filename):
+        anim = Plot.__build_gif(plot_data, data, interval)
         anim.save(filename=f'{filename}.gif', writer='pillow')
         plt.close()
